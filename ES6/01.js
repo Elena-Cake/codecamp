4ff67964334e300182f21ec56282ca3390f5c006
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-08-15 16:32:34 +0300

    4-5 es6

--- ES6/01.js
+++ ES6/01.js
@@ -40,3 +40,14 @@ function freezeObj() {
     return MATH_CONSTANTS.PI;
 }
 const PI = freezeObj();
+
+// 4
+const magic = () => {
+    return new Date();
+};
+//   5
+const myConcat = (arr1, arr2) => {
+    return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
