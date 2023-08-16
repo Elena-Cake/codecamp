0116c0960e5c015edd408febf34f87e5f43daa2a
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-08-16 12:10:06 +0300

    38-39

--- ES6/01.js
+++ ES6/01.js
@@ -50,4 +50,52 @@ const myConcat = (arr1, arr2) => {
     return arr1.concat(arr2);
 };
 
-console.log(myConcat([1, 2], [3, 4, 5]));
+// console.log(myConcat([1, 2], [3, 4, 5]));
+
+
+// 38
+// Setup
+const recordCollection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+        artist: 'Robert Palmer',
+        tracks: []
+    },
+    5439: {
+        albumTitle: 'ABBA Gold'
+    }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value = '') {
+    if (prop === 'tracks' && value !== '') {
+        records[id].tracks ? records[id].tracks.push(value) : records[id].tracks = [value]
+    } else if (value !== '') {
+        records[id] = {
+            ...records[id],
+            [prop]: value
+        }
+    }
+    if (value === '') delete records[id][prop]
+    // console.log(id, records[id])
+}
+
+updateRecords(recordCollection, 2548, "artist", "");
+
+// 39
+const myArray = [];
+let i = 5
+while (i >= 0) {
+    myArray.push(i);
+    i--;
+}
+console.log(myArray)
